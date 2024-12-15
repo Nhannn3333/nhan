6a2cf35e44830e96058c840534327c9a2bc1f821
Nhannn3333 <52200094@student.tdtu.edu.vn>
2024-12-15 23:04:07 +0700

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from models import User
@@ -18,11 +18,9 @@ Base.metadata.create_all(bind=engine)
 # Tạo SessionLocal để tương tác với cơ sở dữ liệu
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Hàm lấy thông tin người dùng dựa trên username
 def get_user_by_username(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
 
-# Hàm tạo người dùng mới
 def create_user(db: Session, displayname: str, username: str, hashed_password: str):
     db_user = User(displayname=displayname, username=username, hashed_password=hashed_password)
     db.add(db_user)
@@ -30,16 +28,12 @@ def create_user(db: Session, displayname: str, username: str, hashed_password: s
     db.refresh(db_user)
     return db_user
 
-# Hàm tăng số lần đăng nhập sai
 def increment_failed_attempts(db: Session, user: User):
     user.failed_attempts += 1
     if user.failed_attempts >= 3:
-        user.is_locked = True  # Khóa tài khoản nếu vượt quá 3 lần
+        user.is_locked = True
     db.commit()
-    db.refresh(user)
 
-# Hàm đặt lại số lần đăng nhập sai về 0
 def reset_failed_attempts(db: Session, user: User):
     user.failed_attempts = 0
     db.commit()
-    db.refresh(user)
