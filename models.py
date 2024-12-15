ffaafcb2af2c8c13e7d20c28cd04b73ec95c4b79
Nhannn3333 <52200094@student.tdtu.edu.vn>
2024-12-15 20:25:47 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,14 @@
+# models.py
 from sqlalchemy import Column, Integer, String
 from database import Base
 
 class User(Base):
     __tablename__ = "users"
-
+    
     id = Column(Integer, primary_key=True, index=True)
     displayname = Column(String, index=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    failed_attempts = Column(Integer, default=0)  # Số lần nhập sai mật khẩu
-    pin_code = Column(String, nullable=True)  # Mã PIN tạm thời
+    
+    failed_attempts = Column(Integer, default=0)  # Số lần đăng nhập sai
+    pin_code = Column(String, nullable=True)  # Mã PIN để reset mật khẩu
