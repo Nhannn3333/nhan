a9aa49dcbe783521cd01fca086d7a7908e45c7ef
Nhannn3333 <52200094@student.tdtu.edu.vn>
2024-12-15 22:17:04 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
+# models.py
 from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    displayname = Column(String, index=True)  # Thêm trường displayname
+    displayname = Column(String, index=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    login_attempts = Column(Integer, default=0)  # Thêm số lần thử đăng nhập
+    failed_attempts = Column(Integer, default=0)  # Thêm trường đếm số lần đăng nhập sai
     is_locked = Column(Boolean, default=False)  # Trạng thái khóa tài khoản
